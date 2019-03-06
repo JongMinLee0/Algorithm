0c44bc5ae3dc1f397cb776385d12c4db9f5b4d96
JongMinLee <1393062@naver.com>
2019-03-07 01:32:04 +0900

    fix1: 수정1단계. while문에 간격과 줄바꿈 규칙추가 필요.

--- Practicd2/src/Solution2448.java
+++ Practicd2/src/Solution2448.java
@@ -1,36 +1,52 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Solution2448 {
+	public static List<String> sTar() {
+		List<String> st = new ArrayList<>();
+		List<String> st2 = new ArrayList<>();
+		st.add("  *  ");
+		st.add(" * * ");
+		st.add("*****");
+
+		return st;
+	}
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		int t1 = N / 3;
-		int t2 = (t1-1)*3;
-		
-		for (int k=0; k<t1 ; k++) {
-			for (int a=0; a<t2; a++)
-				System.out.print(" ");
-			for (int t=0; t<k+1;t++) {
-				System.out.print("  *  ");
-				System.out.print(" ");
+		int t2 = (t1 - 1) * 3;
+		int t = 0;
+		boolean x = true;
+		List<String> str = sTar();
+		List<String> str2 = new ArrayList<>();
+		while (x) {
+			// N을 2로 나눈거 까지 반복한다.
+			if (t == 0) {
+				for (int k = 0; k < 3; k++) {
+					System.out.println(str.get(k));
+					str2.add(str.get(k));
+				}
 			}
-			System.out.print("\n");
-			for (int a=0; a<t2; a++)
-				System.out.print(" ");
-			for (int i=0; i<k+1; i++) {
-				System.out.print(" * * ");
-				System.out.print(" ");
+			t++;
+			if(N==3)
+				break;
+			int f = str2.size();
+			for(int j=0; j<f; j++) {
+					System.out.print(str2.get(j));
+					str2.add(str2.get(j));
+					System.out.print(str2.get(j));
+					str2.add(str2.get(j));
+					if(j==f-1)
+						System.out.print("\n");
+				
 			}
-			System.out.print("\n");
-			for (int a=0; a<t2; a++)
-				System.out.print(" ");
-			for (int j=0; j<k+1; j++) {
-				System.out.print("*****");
-				System.out.print(" ");
-			}
-			System.out.print("\n");
-			t2-=3;
+			if(t==t1-1)
+				x=false;
+			
 		}
-		
+
 	}
 }
