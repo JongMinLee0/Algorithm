9597a75dc33c4fb26db15b009d97ba0f3639fe12
JongMinLee <1393062@naver.com>
2019-03-07 15:37:54 +0900

    fix2: -2448번 추가수정 (알고리즘 미완성)

--- Practicd2/src/Solution2448.java
+++ Practicd2/src/Solution2448.java
@@ -28,25 +28,25 @@ public class Solution2448 {
 				for (int k = 0; k < 3; k++) {
 					System.out.println(str.get(k));
 					str2.add(str.get(k));
+					str2.add("\n");
 				}
 			}
 			t++;
-			if(N==3)
+			if (N == 3)
 				break;
 			int f = str2.size();
-			for(int j=0; j<f; j++) {
+			for (int j = 0; j < f; j++) { // 행바뀜 for문 
+				for (int i = 0; i < 2; i++) { //이거for문 범위 바꿔야함.
 					System.out.print(str2.get(j));
+					if(str2.get(j)=="\n")
+						i=1;
 					str2.add(str2.get(j));
-					System.out.print(str2.get(j));
-					str2.add(str2.get(j));
-					if(j==f-1)
-						System.out.print("\n");
-				
+				}
+
 			}
-			if(t==t1-1)
-				x=false;
-			
-		}
+			if (t == t1 - 1)
+				x = false;
+			}
 
 	}
 }
