988832741985cb025099a39b570fc6f48552913a
JongMinLee <1393062@naver.com>
2019-03-10 19:40:23 +0900

    add : - BaekJoon 11654번 풀이

---
+++ Practicd2/src/Solution11654.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Solution11654 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		char ch = scanner.next().charAt(0);
+		
+		System.out.println((int)ch);
+	}
+}
