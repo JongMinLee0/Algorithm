84f72f0c068a41cd7198eb81f626e8ebf4f0d56e
JongMinLee <1393062@naver.com>
2019-03-10 15:04:13 +0900

    add : - BaekJoon 10039번 문제 풀이

---
+++ Practicd2/src/Solution10039.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Solution10039 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(System.out));
+		int x=0;
+		int sum=0;
+		for(int k=0; k<5; k++) {
+			x = Integer.parseInt(br.readLine());
+			if(x<40)
+				x=40;
+			sum+=x;
+		}
+		int avg = sum/5;
+		wr.write(String.valueOf(avg));
+		
+		wr.flush();
+		wr.close();
+		br.close();
+	}
+}
