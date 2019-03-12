0a16e88e79f5f630dbfbef3a929d66caf98c68ac
JongMinLee <1393062@naver.com>
2019-03-12 18:21:48 +0900

    fix : - BaekJoon 1316 풀이 수정본(완성)

--- Practicd2/src/Solution1316.java
+++ Practicd2/src/Solution1316.java
@@ -5,56 +5,31 @@ import java.util.List;
 public class Solution1316 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		List<String> str = new ArrayList<>();
-		int a1 = 97;
-		char[] ch1 = new char[25];
-		int[] in1 = new int[25];
 		int N = Integer.parseInt(br.readLine());
-		int a = 0;
+		List ina = new ArrayList<>();
 		int count = 0;
-		int result = 0;
-		for (int k = 0; k < ch1.length; k++) {
-			ch1[k] = (char) a1;
-			a1++;
-		}
-		while (a < N) {
-			String st = br.readLine();
-			String[] st1 = st.split("");
-			for (int i = 0; i < ch1.length; i++) {
-				if (ch1[i] == st.charAt(0))
-					in1[i] += 1;
-			}
 
-			for (int j = 0; j < st1.length - 1; j++) {
-				if (st.charAt(j) == st.charAt(j + 1)) {
-					for (int i = 0; i < ch1.length; i++) {
-						if (ch1[i] == st.charAt(j))
-							in1[i] += 1;
-					}
-				} else {
-					for (int i = 0; i < ch1.length; i++) {
-						if (ch1[i] == st.charAt(j + 1)) {
-							in1[i] += 1;
-							if (in1[i] != 1)
-								count = 1;
-						}
+		for (int k = 0; k < N; k++) {
+			boolean result = true;
+			String s = br.readLine();
+			String[] st = s.split("");
 
-					}
-
+			for (int j = 0; j < s.length(); j++) {
+				int pos = s.indexOf(st[j]);
+				while (pos > -1) {
+					ina.add(pos);
+					pos = s.indexOf(st[j], pos + 1);
 				}
+				for (int i = 0; i < ina.size() - 1; i++)
+					if ((int) ina.get(i + 1) - (int) ina.get(i) != 1 && ina.size() != 0)
+						result = false;
+				ina.clear();
 
 			}
-			for (int k = 0; k < st1.length; k++)
-				st1[k] = "";
-			for (int k = 0; k < in1.length; k++)
-				in1[k] = 0;
-			if (count == 0)
-				result++;
-			count = 0;
-			a++;
+			if (result)
+				count++;
 		}
-
-		System.out.println(result);
-		br.close();
+		System.out.println(count);
 	}
+
 }
