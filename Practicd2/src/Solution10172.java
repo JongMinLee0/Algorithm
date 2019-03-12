246a830ed94d9df31b08cb34246aad0e55125846
JongMinLee <1393062@naver.com>
2019-03-12 19:03:02 +0900

    add : - BaekJoon 알고리즘 기초 문제 풀이.

---
+++ Practicd2/src/Solution10172.java
@@ -0,0 +1,12 @@
+
+public class Solution10172 {
+	public static void main(String[] args) {
+		String st = "|\\_/|\n" + 
+				"|q p|   /}\n" + 
+				"( 0 )\"\"\"\\\n" + 
+				"|\"^\"`    |\n" + 
+				"||_/=\\\\__|";
+		System.out.println(st);
+	}
+
+}
