838dd785e1284d82f89a54746c2952dcba5bd322
JongMinLee <1393062@naver.com>
2019-04-08 21:34:21 +0900

    fix : - 시간초과 및 메모리 부족 문제 해결 필요

--- Practicd2/src/BaekJoon/Solution10989.java
+++ Practicd2/src/BaekJoon/Solution10989.java
@@ -1,32 +1,30 @@
 package BaekJoon;
 
 import java.io.*;
-import java.util.Arrays;
 
 public class Solution10989 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		int[] resultArray = new int [N];
-		int[] finalArray = new int [N];
-		int max = 0;
-		for(int i=0; i<N; i++) {
-			resultArray[i] = Integer.parseInt(br.readLine());
-			if(max<resultArray[i])
-				max = resultArray[i];
+		int[] input = new int [10001];
+		for(int i=0; i < N; i++) {
+			input[Integer.parseInt(br.readLine())]++;
 		}
-		int[] count = new int [max];
-		for(int k=0; k<resultArray.length; k++)
-			count[resultArray[k]-1]++;
-		for(int j=1; j<count.length; j++)
-			count[j] += count[j-1];
+		int count = 0;
+		int index = 0;
 		
-		for(int i=resultArray.length-1; i>=0; i--) {
-			finalArray[count[resultArray[i]-1]-1] = resultArray[i];
-			count[resultArray[i]-1]--;
+		while(true) {
+			if(input[index]!=0) {
+				for(int i=0; i < input[index]; i++) {
+					System.out.println(index);
+					count++;
+				}
+				index++;
+			}else {
+				index++;
+			}
+			if(count==N)
+				break;
 		}
-		for(int w=0; w < finalArray.length; w++)
-			System.out.println(finalArray[w]);
-		
 	}
 }
