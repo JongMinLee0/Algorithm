742da7db6776ff4eca0181bb481a4337edece4c9
JongMinLee <1393062@naver.com>
2019-04-10 14:25:48 +0900

    complete : - 문제풀이 완료 / BufferedWriter 사용으로 문제해결.

--- Practicd2/src/BaekJoon/Solution10989.java
+++ Practicd2/src/BaekJoon/Solution10989.java
@@ -1,15 +1,37 @@
 package BaekJoon;
 
 import java.io.*;
-import java.util.*;
+
 public class Solution10989 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(System.out));
+		
 		int N = Integer.parseInt(br.readLine());
-		List<Integer> inPut = new ArrayList<>();
+		int[] input = new int [10001];
 		for(int i=0; i < N; i++) {
-			int index = Integer.parseInt(br.readLine());
-			inPut.add(index, 1);
+			input[Integer.parseInt(br.readLine())]++;
 		}
+		int count = 0;
+		int index = 0;
+		
+		while(true) {
+			if(input[index]!=0) {
+				for(int i=0; i < input[index]; i++) {
+					wr.write(String.valueOf(index));
+					wr.newLine();
+					count++;
+				}
+				index++;
+			}else {
+				index++;
+			}
+			if(count==N)
+				break;
+		}
+		wr.flush();
+		wr.close();
+		br.close();
 	}
 }
+
