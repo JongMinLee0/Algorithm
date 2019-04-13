4e079973237f7d028f5df4b4bada80e69cf35e90
JongMinLee <1393062@naver.com>
2019-04-13 19:46:26 +0900

    add : - BaekJoon 1874번 풀이 아이디어 부재

---
+++ Practicd2/src/BaekJoon/Solution1874.java
@@ -0,0 +1,19 @@
+package BaekJoon;
+
+import java.io.*;
+import java.util.*;
+public class Solution1874 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int testCase = Integer.parseInt(br.readLine());
+		List<Integer> stack = new ArrayList<>();
+		int[] nArray = new int [testCase];
+		
+		for(int i = 0; i < nArray.length; i++) {
+			nArray[i] = Integer.parseInt(br.readLine());
+		}
+		
+		
+		
+	}
+}
