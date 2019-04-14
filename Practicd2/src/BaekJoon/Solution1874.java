d0392686dc470efa497e8758407fab0e7131dd7b
JongMinLee <1393062@naver.com>
2019-04-14 15:04:56 +0900

    fix : - BaekJoon 1874번 풀이 완성

--- Practicd2/src/BaekJoon/Solution1874.java
+++ Practicd2/src/BaekJoon/Solution1874.java
@@ -6,14 +6,45 @@ public class Solution1874 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int testCase = Integer.parseInt(br.readLine());
-		List<Integer> stack = new ArrayList<>();
-		int[] nArray = new int [testCase];
 		
+		List<Integer> stack = new ArrayList<>();
+		List<String> result = new ArrayList<>();
+		int[] nArray = new int [testCase];
+		int[] sArray = new int [testCase];
 		for(int i = 0; i < nArray.length; i++) {
 			nArray[i] = Integer.parseInt(br.readLine());
+			sArray[i]=i+1;
 		}
 		
-		
-		
+		int nindex = 0;
+		int sindex = 0;
+		stack.add(sArray[sindex]);
+		sindex++;
+		result.add("+");
+		while(true) {
+			
+			if(stack.contains(nArray[nindex])) {
+				int num = stack.get(stack.size()-1);
+				
+				if(num==nArray[nindex]) {
+					result.add("-");
+					stack.remove(stack.size()-1);
+					nindex++;
+				}else {
+					System.out.println("NO");
+					System.exit(0);
+					break;
+				}
+			}else {
+				stack.add(sArray[sindex]);
+				sindex++;
+				result.add("+");
+			}
+			if(nindex==sArray.length) {
+				break;
+			}
+		}
+		for(int k = 0; k < result.size(); k++)
+			System.out.println(result.get(k));
 	}
 }
