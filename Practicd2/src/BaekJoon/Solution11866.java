fa8725969db9b0e577e6c1ccd3d0b1cf5069fa3c
JongMinLee <1393062@naver.com>
2019-04-20 00:00:31 +0900

    fix : - BaekJoon 11866번 풀이

--- Practicd2/src/BaekJoon/Solution11866.java
+++ Practicd2/src/BaekJoon/Solution11866.java
@@ -2,44 +2,41 @@ package BaekJoon;
 
 import java.io.*;
 import java.util.*;
-// runtime error
+
 public class Solution11866 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] input = br.readLine().split(" ");
-
 		int N = Integer.parseInt(input[0]);
 		int K = Integer.parseInt(input[1]) - 1;
-		List<Integer> result = new ArrayList<Integer>();
+		int[] result = new int[N];
 		List<Integer> josep = new ArrayList<Integer>();
-
-		for (int i = 0; i < N; i++)
-			josep.add(i + 1);
-
-		int index = K;
-		int count = 0;
-		while (true) {
-			result.add(josep.get(index));
+		for (int i = 0; i < N; i++) {
+			josep.add(i+1);
+		}
+		
+		int index =K;
+		int index2 = 0;
+		
+		while(josep.size()>0) {
+			
+			result[index2] = josep.get(index);
 			josep.remove(index);
 			index += K;
-			if (index >= josep.size()) {
-				index = index - josep.size();
-				if (index == josep.size()) {
-					for (int k = 0; k < index; k++) {
-						result.add(josep.get(0));
-						josep.remove(0);
-					}
+			while(index>=josep.size()) {
+				index -= josep.size();
+				if(josep.size()==0)
 					break;
-				}
 			}
-			count++;
-			if (count == N)
-				break;
+			index2++;
 		}
+
+		
 		System.out.print("<");
-		for(int j=0; j<result.size()-1; j++) {
-			System.out.print(result.get(j)+", ");
+		for (int j = 0; j < result.length - 1; j++) {
+			System.out.print(result[j] + ", ");
 		}
-		System.out.print(result.get(result.size()-1)+">");
+		System.out.print(result[result.length - 1] + ">");
+
 	}
 }
