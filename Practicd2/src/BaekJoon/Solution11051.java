9e85eb41c51a4c9f04633653faa85a565771c09c
JongMinLee <1393062@naver.com>
2019-05-02 17:43:30 +0900

    add : Didn't solve the problem
    The result is wrong. because the size of integer type is small.

---
+++ Practicd2/src/BaekJoon/Solution11051.java
@@ -0,0 +1,24 @@
+package BaekJoon;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Solution11051 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		HashMap<Integer, Integer> fac = new HashMap<Integer, Integer>();
+		
+		
+		
+		
+		
+		
+		
+	}
+}
