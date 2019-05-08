509b559bc8713fe6364c887fc8b1ab1baa1e08ca
JongMinLee <1393062@naver.com>
2019-05-09 00:08:41 +0900

    docs : Complete the No.1934 problem solving

--- Practicd2/src/BaekJoon/Solution1934.java
+++ Practicd2/src/BaekJoon/Solution1934.java
@@ -10,15 +10,31 @@ public class Solution1934 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int testCase = Integer.parseInt(br.readLine());
-		
-		String[] input = new String[testCase*2];
-		
-		for (int i = 0; i < testCase*2; i++) {
+
+		int[] input = new int[testCase * 2];
+
+		for (int i = 0; i < testCase * 2; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			input[i] = st.nextToken();
-			input[i+1] = st.nextToken();
+			input[i] = Integer.parseInt(st.nextToken());
+			input[i + 1] = Integer.parseInt(st.nextToken());
 			i++;
 		}
-		
+		for (int k = 0; k < testCase * 2; k++) {
+			int first = input[k];
+			int second = input[k + 1];
+
+			int max = Math.max(first, second);
+			int min = Math.min(first, second);
+			for (int w = 1; w <= min; w++) {
+				if (min % w == 0) {
+					if ((max * w) % min == 0) {
+						System.out.println(w * max);
+						break;
+					}
+
+				}
+			}
+			k++;
+		}
 	}
 }
